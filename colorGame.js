ef4a70798e70b903b69f045a988b8fba3b3e74b7
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-03-17 23:10:10 -0700

    logic for checking clicked color and our target color implemented

--- colorGame.js
+++ colorGame.js
@@ -8,7 +8,22 @@ var colors = [
 ];
 
 var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++){
 	squares[i].style.backgroundColor = colors[i];
+
+	squares[i].addEventListener("click", function() {
+		var clickedColor = this.style.backgroundColor;
+
+		if(clickedColor=== pickedColor){
+			alert("Right");
+		}else{
+			alert("wrong");
+		}
+	})
 }
+
