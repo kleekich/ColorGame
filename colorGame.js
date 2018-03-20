87e69b552dd7e9411cabd17c4d6b8b3b3f6d1c9f
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-03-19 23:14:02 -0700

    color change on user's correct answer implementing

--- colorGame.js
+++ colorGame.js
@@ -1,16 +1,10 @@
-var colors = [
-"rgb(255, 0, 0)",
-"rgb(255, 255, 0)",
-"rgb(0, 255, 0)",
-"rgb(0, 255, 255)",
-"rgb(0, 0, 255)",
-"rgb(255, 0, 255)"
-];
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.getElementById("message");
+var h1 = document.queryselector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -41,4 +35,26 @@ function pickColor(){
 
 	var random = Math.floor(Math.random() * colors.length);
 	return colors[random];
+}
+
+function generateRandomColors(num){
+	var arr = [];
+
+	for(var i = 0; i <num; i++){
+		//get random color push into arr
+		arr[i] = randomColor();
+	}
+
+
+	return arr;
+
+}
+
+function randomColor() {
+	//pick red
+	var r = Math.floor(Math.random()*256);
+	//pick green
+	var g = Math.floor(Math.random()*256);
+	var b = Math.floor(Math.random()*256);
+	return "rgb("+r+", "+g+", "+b+")";
 }
